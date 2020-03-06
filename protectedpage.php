71dfe6a3c8674a8a2c8efc60d7c4f13b1f14f1ff
TBM Productions <61883288+tbm-productions@users.noreply.github.com>
2020-03-06 22:48:20 +0000

    Create protectedpage.php
    Protected Page, can only be accessed once the user has entered the correct password.

---
+++ protectedpage.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if (!isset($_SESSION["authed"]) || $_SESSION["authed"] === false) {
+    header("location:/protected-page/auth");
+    exit(307);
+}
+?>
+<!DOCTYPE html>
+<html lang="en-GB">
+    <head>
+        <title> Protected Page - TBM Productions </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Concert+One|Boogaloo">
+    </head>
+    <body>
+        <!-- Viedo Background -->
+        <video autoplay muted loop id="bgvideo">
+            <source src="/library/site/assets/videos/lines.mp4" type="video/mp4">
+            Your browser does not support HTML5 video.
+        </video>
+        <!-- Form with full screen fallback & opacity -->
+        <div class="background">
+            <div class="holder">
+                <div class="labels">
+                    <h1> Welcome </h1>
+                    <h3 style="font-family: arial;"> Welcome to the password protected page. You can access this page until you click logout or the browser session ends.  </h3>
+                </div>
+                <div class="input-group">
+                    <button onclick="window.location.replace('/protected-page/logout')"> Logout </button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
