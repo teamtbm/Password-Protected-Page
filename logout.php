8a096c9849da90e3e2f768ff0b6ac0eb469ef759
TBM Productions <61883288+tbm-productions@users.noreply.github.com>
2020-03-06 23:14:13 +0000

    Update logout.php

--- logout.php
+++ logout.php
@@ -9,6 +9,6 @@ $_SESSION = array();
 session_destroy();
  
 // Redirect to login page
-header("location:https://developer.tbmproduction.com/protected-page/auth");
+header("location:auth.php");
 exit;
 ?>
